9d55fb2990cf6af8a5a401cc5244d93df8c755c6
vadim-os <osinsky_vadim@yahoo.com>
2019-12-17 19:20:54 +0200

    Updates

---
+++ precache-manifest.f812ddab5d79162a9ce668346fb2df6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d702b78f0c0b0411228e8176239fbc",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "391664f34662568e93a4",
+    "url": "/react_people-table/static/css/main.4ebb6439.chunk.css"
+  },
+  {
+    "revision": "8c62e9e39566e04c3c1e",
+    "url": "/react_people-table/static/js/2.263e448f.chunk.js"
+  },
+  {
+    "revision": "391664f34662568e93a4",
+    "url": "/react_people-table/static/js/main.779a8228.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
